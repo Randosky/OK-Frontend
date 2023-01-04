29a16af9f6e53d7da4e0e68ec4bed08223e8d97a
Ovinkin Kirill <103418173+Randosky@users.noreply.github.com>
2023-01-04 21:28:23 +0500

    Первичная настройка

---
+++ frontend_project/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from frontend_project.models import Profession
+
+
+def show_page(request):
+    data = {"profession": Profession.objects.get(id=1)}
+    return render(request, "frontend.html", data)
